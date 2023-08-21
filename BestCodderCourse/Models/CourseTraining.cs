c7a88e75a022ed0318b6d9a5b908e94cf060ce2a
sena <snbyrktr@hotmail.com>
2023-08-21 16:44:46 +0300

    lifecycle-onlnitialized

---
+++ BestCodderCourse/Models/CourseTraining.cs
@@ -0,0 +1,13 @@
+﻿namespace BestCodderCourse.Models
+{
+    public class CourseTraining
+    {
+        public int Id { get; set; }
+
+        public string CourseName { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
